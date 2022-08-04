72ba23a7cd8d206a4fef25eaf75938c295885604
Marty Sloan <marty@karmatek.io>
2022-08-04 18:02:21 -0400

    Add Projects config

---
+++ projects/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.projects, name="projects"),
+    path('<str:pk>', views.project, name="project"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+projects_list = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
+
+def projects(request):
+    return render(request, 'projects/index.html', {'projects': projects_list})
+
+
+def project(request, pk):
+    project_object = None
+
+    for project_item in projects_list:
+        if project_item['id'] == pk:
+            project_object = project_item
+
+    context = {
+        'project': project_object
+    }
+
+    return render(request, 'projects/show.html', context)
